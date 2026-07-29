8537aafeb9d00200e922e41980dade95f159b378
tristanworkman <sungjinworkman@gmail.com>
2026-07-29 10:41:07 -0500

    I added links to references and datasets that we used.

--- finetuned_roberta_class.py
+++ finetuned_roberta_class.py
@@ -46,6 +46,24 @@ def convert_label_to_numb(pred_label):
 
 class FTRobertaModel:
     def __init__(self):
+        #This is the hugging face pretrained model we used.
+        # @inproceedings
+        #
+        # {barbieri - etal - 2022 - xlm,
+        #  title = "{XLM}-{T}: Multilingual Language Models in {T}witter for Sentiment Analysis and Beyond",
+        # author = "Barbieri, Francesco  and
+        # Espinosa
+        # Anke, Luis and
+        #       Camacho - Collados, Jose
+        # ",
+        # booktitle = "Proceedings of the Thirteenth Language Resources and Evaluation Conference",
+        # month = jun,
+        # year = "2022",
+        # address = "Marseille, France",
+        # publisher = "European Language Resources Association",
+        # url = "https://aclanthology.org/2022.lrec-1.27",
+        # pages = "258--266"
+        # }
         self.model = AutoModelForSequenceClassification.from_pretrained("roberta_sentiment_model") #Load in my model and the tokenizer.
         self.tokenizer = AutoTokenizer.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis", use_fast = False)
         self.pred_number = None
